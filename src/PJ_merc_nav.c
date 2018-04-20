24dc5d18582ffeb8a7914fc99a6df83366d85671
Alessandro Gentili <agentili@navionics.com>
2018-04-20 12:00:04 +0200

    Fix name name in funcion
    Added old tests as commented part to be used as references in the future

--- src/PJ_merc_nav.c
+++ src/PJ_merc_nav.c
@@ -5,7 +5,7 @@
 PROJ_HEAD(merc_nav, "Mercator Navionics") "\n\tCyl, Sph&Ell\n\tlat_ts=";
 
 #define EPS10 1.e-10
-#define KC = 1.00676429271698
+#define KC 1.00676429271698
 
 static XY e_forward (LP lp, PJ *P) {          /* Ellipsoidal, forward */
     XY xy = {0.0,0.0};
@@ -51,7 +51,7 @@ static LP e_inverse (XY xy, PJ *P) {          /* Ellipsoidal, inverse */
 // }
 
 
-PJ *PROJECTION(merc) {
+PJ *PROJECTION(merc_nav) {
     double phits=0.0;
     int is_phits;
 
@@ -78,3 +78,48 @@ PJ *PROJECTION(merc) {
     return P;
 }
 
+
+// #ifdef PJ_OMIT_SELFTEST
+// int pj_nav_merc_selftest (void) {return 0;}
+// #else
+// 
+// int pj_nav_merc_selftest (void) {
+    // double tolerance_lp = 1e-10;
+    // double tolerance_xy = 1e-7;
+// 
+    // char e_args[] = {" -w6 +proj=nav_merc +ellps=intl"};
+// 
+    // LP fwd_in[] = {
+        // { 950594.539, 5968306.230 },
+        // { 1178796.03, 5276463.78 },
+        // { 18671158.340, -5945578.371 },
+        // { 20038296.88, -18765191.35 }
+    // };
+// 
+    // XY e_fwd_expect[] = {
+        // { 8.5390000000, 47.3515666667 },
+        // { 10.5888881683, 42.9574966432 },
+        // { 167.7192687988, -47.2126350403 },
+        // { 179.9998888889, -84.0 },
+    // };
+// 
+    // XY inv_in[] = {
+        // { 8.5390000000, 47.3515666667 },
+        // { 10.5888881683, 42.9574966432 },
+        // { 167.7192687988, -47.2126350403 },
+        // { 180.0, -84.0 }
+    // };
+// 
+    // LP e_inv_expect[] = {
+        // { 950594.539, 5968306.230 },
+        // { 1178796.03, 5276463.78 },
+        // { 18671158.340, -5945578.371 },
+        // { 20038296.883, -18765191.347 },
+    // };
+// 
+// 
+    // return pj_generic_selftest (e_args, 0, tolerance_xy, tolerance_lp, 4, 4, fwd_in, 0, e_fwd_expect, inv_in, 0, e_inv_expect );
+// }
+// 
+// 
+// #endif
